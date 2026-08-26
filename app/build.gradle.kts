ac562e8a90b563709cb1feb440ed9e51a77784aa
prabhukumarp71-cmyk <prabhukumarp71@gmail.com>
2026-08-26 07:38:08 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,9 +45,13 @@ android {
       isMinifyEnabled = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
-    }
-    }
-  }
+        }
+      }
+      
+    
+  
+    
+  
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
